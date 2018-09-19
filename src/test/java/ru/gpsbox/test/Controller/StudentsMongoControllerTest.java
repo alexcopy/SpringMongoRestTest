b16fdacbbb6c11e8e20452406a889d0cdcd3bf2e
Alex Red <alexcopy@mail.ru>
2018-09-19 11:23:20 +0100

    Add Test for mongo Controller

---
+++ src/test/java/ru/gpsbox/test/Controller/StudentsMongoControllerTest.java
@@ -0,0 +1,44 @@
+package ru.gpsbox.test.Controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentsMongoControllerTest {
+
+    StudentsMongoController studMongContr;
+    @Before
+    public void setUp() throws Exception {
+//        studMongContr = new StudentsMongoController();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getStudentById() {
+    }
+
+    @Test
+    public void getStudentByName() {
+    }
+
+    @Test
+    public void deleteStudentById() {
+    }
+
+    @Test
+    public void deleteStudentByName() {
+    }
+
+    @Test
+    public void updateStudent() {
+    }
+
+    @Test
+    public void insertStudent() {
+    }
+}
