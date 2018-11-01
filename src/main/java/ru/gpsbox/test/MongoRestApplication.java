e68905520dedbc0e843dc8d210ba3c074bef544e
Alex Red <alexcopy@mail.ru>
2018-11-01 10:31:46 +0000

    add pretty log

--- src/main/java/ru/gpsbox/test/MongoRestApplication.java
+++ src/main/java/ru/gpsbox/test/MongoRestApplication.java
@@ -2,15 +2,66 @@ package ru.gpsbox.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 @SpringBootApplication
 @EnableScheduling
-@EnableMongoRepositories(basePackages={"ru.gpsbox.test.persistance.mongo"})
+@EnableMongoRepositories(basePackages = {"ru.gpsbox.test.persistance.mongo"})
 public class MongoRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(MongoRestApplication.class);
+    private final Environment env;
+    public MongoRestApplication(Environment env) {
+        this.env = env;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(MongoRestApplication.class, args);
+
+        SpringApplication app = new SpringApplication(MongoRestApplication.class);
+//        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
     }
 }
